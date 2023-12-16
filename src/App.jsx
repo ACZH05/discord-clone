138c15edcbb85de911f114e63d2b7c938fa19d76
Alfred Chin <alfredchin2005@gmail.com>
2023-12-16 15:10:28 +0800

    Add login routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import AuthPage from "./pages/AuthPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AuthPage from "./pages/LoginPage"
 
 export default function App() {
   return (
     <div>
-      <AuthPage />
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index path="/" />
+            <Route path="/login" element={<AuthPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
