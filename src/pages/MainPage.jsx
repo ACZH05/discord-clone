a1e665e64b3a0310324d410ae05f6a243ea48566
Alfred Chin <alfredchin2005@gmail.com>
2024-01-08 20:25:21 +0800

    Add main menu template

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,19 @@
+function MainPage() {
+  return (
+    <div className="overflow-hidden">
+        <div className="flex h-screen">
+            <div className="w-[72px] bg-[#1e1f22]">
+                Hello
+            </div>
+            <div className="w-[240px] bg-[#2b2d31]">
+                Hello
+            </div>
+            <div className="">
+                Hello
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MainPage
